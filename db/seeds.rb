ca6c10279e9a34ee8d2e7d76f72e5a0f8929e1c3
Jukka Kalliolehto <jugezi@KIVI14AL-JUKKA.local>
2014-06-26 13:10:55 +0300

    Add books.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create!([{
+title: "Kirja1",
+author: "Tekija1",
+description: "Blaablaa1",
+amazon_id: "123321123",
+rating: 5,
+finished_on: 10.days.ago
+},
+{
+title: "Kirja2",
+author: "Tekija2",
+description: "Blaablaa2",
+amazon_id: "9879873937373",
+rating: 3,
+finished_on: 1.day.ago
+},
+{
+title: "Kirja3",
+author: "Tekija3",
+description: "Blaablaa3",
+amazon_id: "9879873937323",
+rating: 1,
+finished_on: nil
+}])
+
+p "Created #{Book.count} books"
