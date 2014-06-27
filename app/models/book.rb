ddc61f2539c9cd844223bf32b96f294f6c5d407b
Jukka Kalliolehto <jugezi@KIVI14AL-JUKKA.local>
2014-06-27 15:45:55 +0300

    Improved search with keywords.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,16 +2,16 @@ class Book < ActiveRecord::Base
 
   scope :finished, -> { where.not(finished_on: nil) }
   scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%" ) if keyword.present? }
 
-  def self.search(keyword)
-    if keyword.present?
-      where(title: keyword)
-    else
-      all
-    end
-  end
+  before_save :set_keywords
 
   def finished?
     finished_on.present?
   end
+
+  protected
+    def set_keywords
+      self.keywords = [title, author, description].map(&:downcase).join(' ')
+    end
 end
